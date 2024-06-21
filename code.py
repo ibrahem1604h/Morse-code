bd760cd0ea3d298b785188c91d72b2a295858084
ibrahem1604h <ibrahim28283@gmail.com>
2024-06-21 06:22:29 +0400

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 import string
 alphabets = string.ascii_lowercase+string.ascii_uppercase
-words=input("enter a word (enter to decode): ")
+words=input("enter a word (enter to decryped): ")
 def encrypt(word,number):
   encrypted_word=""
   for letter in word:
@@ -16,21 +16,21 @@ def encrypt(word,number):
       encrypted_word+=letter
   print(f" the encrtpted word is :  {encrypted_word}")
 def orignal(word,number):
-  encrypted_word=""
+  decrypted_word=""
   for letter in word:
     if letter.lower() in alphabets:
       original_position = alphabets.index(letter)
       new_position = original_position - number
       if new_position >= 26:
         new_position=  new_position - 26
-      encrypted_word+=alphabets[new_position]
+      decrypted_word+=alphabets[new_position]
       if letter.isupper():
-        encrypted_word=encrypted_word.upper()
+        decrypted_word=decrypted_word.upper()
     else:
-      encrypted_word+=letter
-  print(f" the orignal word is :  {encrypted_word}")
+      decrypted_word+=letter
+  print(f" the orignal word is :  {decrypted_word}")
 if words:
- 
+
   numbers=int(input("enter a number: "))
   encrypt(words,numbers)
 else:
